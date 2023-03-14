df0114ce1aeaf30e9d4045a02c94392c52e341db
Zalman Zoteev <sz.zoteev@gmail.com>
2023-03-14 20:18:20 +0200

    Initial Commit

---
+++ TwoView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoView
+//
+//  Created by Zalman Zoteev on 14/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
